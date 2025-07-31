3c5fd9a2d8491f58c1fdb17e4bcd006b198351db
macauley <luiz.ramos.8892@ga.ita.br>
2025-07-31 00:14:51 -0300

    Starts development of database features using SQLite

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,26 @@
 import { Text, View } from "react-native";
-import * as SQLite from 'expo-sqlite'
+import * as SQLite from 'expo-sqlite';
+import {useState, useEffect} from 'react';
+
+const db = await SQLite.openDatabaseAsync('absences.db');
 
 export default function Index() {
+    const [disciplines, setDisciplines] = useState<string[]>([]);
+    const [absences, setAbsences] = useState<number[]>([]);
+    const [max, setMax] = useState<number[]>([]);
+
+    useEffect(() => {
+        const runAsync = async () => {
+            const result = await db.execAsync('CREATE TABLE IF NOT EXISTS faltas ' +
+                '(name TEXT PRIMARY KEY NOT NULL, numAbsences INTEGER NOT NULL, maxAbsences INTEGER)');
+        }
+
+        runAsync();
+    }, []);
+
   return (
     <View className={"flex-1 flex-start pt-20 pl-4"} >
-      <Text className={"font-bold text-black text-5xl"}>Welcome!</Text>
+      <Text className={"font-bold text-black text-5xl"}>Faltas:</Text>
     </View>
   );
 }
